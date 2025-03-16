f3abdf8963586dded67e92aacfe44b0a4e8b8f63
Hante990 <alonsofdez2004@gmail.com>
2025-03-15 21:48:09 -0600

    Jordy

---
+++ Gauss_Jordan/GaussJordan.java
@@ -0,0 +1,12 @@
+package Gauss_Jordan;
+
+public class GaussJordan {
+
+    public static void main(String[] args) {
+       System.out.println("GAUSS-JORDAN");
+        double[][] matriz = 
+        {   {4,2,-2,1},
+            {10,4,4,-4},
+            {6,2,2,5}    };
+        
+    }}
