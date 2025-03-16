054d891f5e7c317590c1e9a956e53e5b42e836c9
Hante990 <alonsofdez2004@gmail.com>
2025-03-15 18:10:14 -0600

    Commit eliminacion

---
+++ Eliminacion_Gaussiana/Ejercicio1.java
@@ -0,0 +1,14 @@
+package Eliminacion_Gaussiana;
+
+
+public class Ejercicio1 {
+
+      public static void main(String[] args) {
+        int n = 3; // Número de incógnitas
+        double[][] matrix = {
+                {2.0, 1.0, -1.0, 8.0},
+                {-3.0, -1.0, 2.0, -11.0},
+                {-2.0, 1.0, 2.0, -3.0}
+        };
+    }
+}
